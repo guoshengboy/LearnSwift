3357606aaca419d748642e0977a46b5f34413e11
曹国盛 <guosheng.cao@easyhospital.cn>
2017-05-10 16:02:51 +0800

    基础部分差个错误处理      还是要看swift 初见

--- 基础部分/基础部分/ViewController.swift
+++ 基础部分/基础部分/ViewController.swift
@@ -26,13 +26,26 @@ class ViewController: UIViewController {
         let pi1 = 3 + 0.14159;
         print(pi1);
         
+        assert(pi1>100, "必须大于100 不大于100就要崩溃  你试试看")
 
-
+        self.assert1();
         self.optionals();
         self.tuples();
         
     }
     
+    
+    /// 断言 (用于差错)
+    func assert1() -> () {
+        //当条件可能为假时使用断言，但是最终一定要保证条件为真，这样你的代码才能继续运行。断言的适用情景:
+        //整数类型的下标索引被传入一个自定义下标实现，但是下标索引值可能太小或者太大。 • 需要给函数传入一个值，但是非法的值可能导致函数不能正常执行。
+          //  • 一个可选值现在是 nil ，但是后面的代码运行需要一个非 nil 值。
+        let pi1 = 3 + 0.14159;
+        print(pi1);
+         //release 断言失效
+        assert(pi1>100, "必须大于100 不大于100就要崩溃  你试试看")
+    }
+
     
     
     ///可选类型
@@ -42,16 +55,27 @@ class ViewController: UIViewController {
         var abc = Int("123");
         abc = nil;
         if  true{
-            //当打印可选类型是 使用！(！就是告诉编译器我这个一定有值不用在写默认值了)时必须确定abc有值 否则运行报错
-            print(abc!);
             
             //可选类型 默认值  没有空格报错 坑啊
-            print(abc ?? 123);
+            print(abc ?? 11111);
+            
+            //当打印可选类型是 使用！(！就是告诉编译器我这个一定有值不用在写默认值了)时必须确定abc有值 否则运行报错
+//            print(abc!);
         }
-    }
+        
+        
+        //可选绑定（如果把可选类型的包含就把值赋给一个临时常量或者变量 如果成功赋值，就能直接使用这个被复制的变量或常量  一般用于if  while等地方）
+        if  let aAbc = abc{
+            
+            print("the \'\(String(describing: abc))\' have a value of \(aAbc)");
+        }else{
+         
+            print("abc是nil")
+        }
+}
 
     
-    /// 元祖
+    /// 元组
     func tuples() -> () {
         
         //元组tuples创建
@@ -73,7 +97,6 @@ class ViewController: UIViewController {
         //可以给元组元素命名
         let peoplePlay = (xiaohong:12, xiaoming:15);
         print(peoplePlay.xiaoming)
-        
         
     }
 
